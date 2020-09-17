8f0e99f0d23771e471ea9496e39b860a7baeef92
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-17 21:37:35 +0200

    Grid added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Grid from "../../2048game/src/Grid";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      rows: 9,
+      columns: 9,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Grid rows={this.state.rows} columns={this.state.columns} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
