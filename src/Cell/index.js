515471fa3ca04cf1bacda30c830b474bc22713ee
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-21 00:16:32 +0200

    Color codes added

--- src/Cell/index.js
+++ src/Cell/index.js
@@ -3,12 +3,47 @@ import React from "react";
 const Cell = (props) => {
   let cell = () => {
     if (props.data.value !== 0) {
-      return <div className="cell"> {props.data.value}</div>;
-    }else{
-      return <div className="cell"></div>
+      return (
+        <div
+          className="cell"
+          style={{ background: getColor(props.data.value) }}
+        >
+          {" "}
+          {props.data.value}
+        </div>
+      );
+    } else {
+      return <div className="cell"></div>;
     }
   };
   return cell();
+};
+
+const getColor = (value) => {
+  switch (value) {
+    case 2:
+      return "#EBDCD0";
+    case 4:
+      return "#E9DBBA";
+    case 8:
+      return "#E9A067";
+    case 16:
+      return "#F08151";
+    case 32:
+      return "#F2654F";
+    case 64:
+      return "#F1462C";
+    case 128:
+      return "#E7C65E";
+    case 256:
+      return "#E8C350";
+    case 512:
+      return "#E8BE40";
+    case 1024:
+      return "#E7B723";
+    case 2048:
+      return "#C2B3A3";
+  }
 };
 
 export default Cell;
