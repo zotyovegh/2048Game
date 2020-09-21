d78094377c2c9b68f931c8b3c6fb8e6fe534bab2
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-21 19:54:51 +0200

    Updates

---
+++ precache-manifest.bf10b5a4be566f25a591b7404e822f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a301aec64fe97cd67d2f13d28714b200",
+    "url": "/2048Game/index.html"
+  },
+  {
+    "revision": "e52d9291531d3b6af0c8",
+    "url": "/2048Game/static/css/main.80a09d54.chunk.css"
+  },
+  {
+    "revision": "21489d28139661d17c5f",
+    "url": "/2048Game/static/js/2.ce19322a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048Game/static/js/2.ce19322a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52d9291531d3b6af0c8",
+    "url": "/2048Game/static/js/main.9d278ed0.chunk.js"
+  },
+  {
+    "revision": "e9de05b2a52e3495fbf1",
+    "url": "/2048Game/static/js/runtime-main.706fd7ea.js"
+  }
+]);
